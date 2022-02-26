2accc9aa4dc7d9ccf0a7f67cf279fcaf767dd9d1
Tiger <pongpang1587@hotmail.co.th>
2022-02-26 21:42:57 +0700

    updateDayInHomepage

--- lib/Homepage/tabbar.dart
+++ lib/Homepage/tabbar.dart
@@ -15,11 +15,7 @@ class tabbar extends StatefulWidget {
 }
 
 class _tabbarState extends State<tabbar> {
-  final List<String> names = <String>[
-    'Fix my car',
-    'Clean bed room',
-    'buy PS5'
-  ];
+  final List<String> names = <String>[];
   final List<int> icon = <int>[1, 2, 3];
 
   TextEditingController nameController = TextEditingController();
