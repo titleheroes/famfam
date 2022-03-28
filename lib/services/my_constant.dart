44f1bcf7e2c523298ee4bb120245fe2328706578
Apichaya-Jithaisong <17041.mnr@gmail.com>
2022-03-28 20:32:45 +0700

    IP Con

--- lib/services/my_constant.dart
+++ lib/services/my_constant.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 class MyConstant {
   static String domain =
-      'http://79d1-2405-9800-ba00-11c-e44f-477f-789d-2d58.ngrok.io';
+      'http://0e73-2405-9800-ba00-11c-755f-e529-9cfd-2182.ngrok.io';
 }
