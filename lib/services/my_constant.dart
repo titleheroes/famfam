454432efb0bdc92c97f4d96fe49a3182b7b7eb16
titleheroes <title13260@gmail.com>
2022-05-11 23:27:57 +0700

    local changes

--- lib/services/my_constant.dart
+++ lib/services/my_constant.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 class MyConstant {
-  static String domain = 'https://famfam-app.000webhostapp.com';
+  static String domain = 'http://9c51-49-228-101-183.ngrok.io';
 }
